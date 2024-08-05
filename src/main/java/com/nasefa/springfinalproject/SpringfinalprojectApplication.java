4d106dfab4194f88489ba895e4b0d4517e6a9300
SebastianVergaraVelasquez <sv.velasquez36@gmail.com>
2024-08-05 08:20:17 -0500

    refactor: Limpieza SpringfinalprojectApplication

--- src/main/java/com/nasefa/springfinalproject/SpringfinalprojectApplication.java
+++ src/main/java/com/nasefa/springfinalproject/SpringfinalprojectApplication.java
@@ -1,20 +1,11 @@
 package com.nasefa.springfinalproject;
 
-// import java.util.List;
-
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// import com.nasefa.springfinalproject.domain.services.gamma.IGamma;
-// import com.nasefa.springfinalproject.domain.repositories.ProductRepository;
-// import com.nasefa.springfinalproject.persistence.entities.Gamma;
-
 
 @SpringBootApplication
 public class SpringfinalprojectApplication implements CommandLineRunner{
@@ -31,25 +22,13 @@ public class SpringfinalprojectApplication implements CommandLineRunner{
             @SuppressWarnings("null")
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("http://localhost:4200","http://127.0.0.1:5500",
-                        "http://localhost:8080").allowedMethods("").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200","http://127.0.0.1:5500",
+                        "http://localhost:8080").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
 
 	@Override
 	public void run(String... args) throws Exception {
-		// gammaList();
 	}
-	
-	// @Transactional
-	// public void gammaList(){
-	// 	List<Gamma> gammas = gammaService.findAllWithProducts("a");
-
-	// 	gammas.stream().forEach(gamma -> {
-	// 		System.out.println(gamma.getProducts());
-	// 		System.out.println(gamma);
-	// 	});
-	// }
-
 }
